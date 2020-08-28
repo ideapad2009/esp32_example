e3ff074a13f2dfd7497297823c87edf23d1b1056
lin <857413240@qq.com>
2020-08-28 07:12:31 -0700

    timer

--- examples/system/esp_timer/main/esp_timer_example_main.c
+++ examples/system/esp_timer/main/esp_timer_example_main.c
@@ -35,6 +35,7 @@ void app_main()
     };
 
     esp_timer_handle_t periodic_timer;
+    printf(">>>>>>>>1>>>>>>>>创建周期性定时器\n");
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     /* The timer has been created but is not running yet */
 
@@ -45,6 +46,7 @@ void app_main()
             .name = "one-shot"
     };
     esp_timer_handle_t oneshot_timer;
+    printf(">>>>>>>>1>>>>>>>>创建单次定时器\n");
     ESP_ERROR_CHECK(esp_timer_create(&oneshot_timer_args, &oneshot_timer));
 
     /* Start the timers */
@@ -54,24 +56,24 @@ void app_main()
 
     /* Print debugging information about timers to console every 2 seconds */
     for (int i = 0; i < 5; ++i) {
+        printf("|------------------当前运行的定时器:%d--------------------\n",i);
         ESP_ERROR_CHECK(esp_timer_dump(stdout));
+        printf("--------------------------------------|\n");
         usleep(2000000);
     }
 
-    /* Timekeeping continues in light sleep, and timers are scheduled
-     * correctly after light sleep.
-     */
-    ESP_LOGI(TAG, "Entering light sleep for 0.5s, time since boot: %lld us",
-            esp_timer_get_time());
+    
+    // esp_light_sleep_start();
+    // ESP_LOGI(TAG, "进入轻度睡眠%lld us",esp_timer_get_time());
 
-    ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(500000));
-    esp_light_sleep_start();
+    // ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(500000));
+    // ESP_LOGI(TAG, ">>>>>>>>>>>>唤醒定时器启动%lld us",esp_timer_get_time());
 
-    ESP_LOGI(TAG, "Woke up from light sleep, time since boot: %lld us",
-                esp_timer_get_time());
+    // ESP_LOGI(TAG, ">>>>>>>>>>>>Woke up from light sleep, time since boot: %lld us",
+    // esp_timer_get_time());
 
-    /* Let the timer run for a little bit more */
-    usleep(2000000);
+    // /* Let the timer run for a little bit more */
+    // usleep(2000000);
 
     /* Clean up and finish the example */
     ESP_ERROR_CHECK(esp_timer_stop(periodic_timer));
@@ -83,18 +85,18 @@ void app_main()
 static void periodic_timer_callback(void* arg)
 {
     int64_t time_since_boot = esp_timer_get_time();
-    ESP_LOGI(TAG, "Periodic timer called, time since boot: %lld us", time_since_boot);
+    ESP_LOGI(TAG, ">>>>>>>>>>>>>>111定期定时器调用，时间自启动: %lld us\n", time_since_boot);
 }
 
 static void oneshot_timer_callback(void* arg)
 {
     int64_t time_since_boot = esp_timer_get_time();
-    ESP_LOGI(TAG, "One-shot timer called, time since boot: %lld us", time_since_boot);
+    ESP_LOGE(TAG, ">>>>>>>>>>>>>>222单次定时器: %lld us", time_since_boot);
     esp_timer_handle_t periodic_timer_handle = (esp_timer_handle_t) arg;
     /* To start the timer which is running, need to stop it first */
     ESP_ERROR_CHECK(esp_timer_stop(periodic_timer_handle));
+    ESP_LOGE(TAG, ">>>>>>>>>>>>>>333停止周期性定时>>>>>>>\n");
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer_handle, 1000000));
     time_since_boot = esp_timer_get_time();
-    ESP_LOGI(TAG, "Restarted periodic timer with 1s period, time since boot: %lld us",
-            time_since_boot);
+    ESP_LOGE(TAG, "重启周期性定时 1 S,  %lld us",time_since_boot);
 }
